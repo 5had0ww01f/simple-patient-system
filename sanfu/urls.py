4ed2b80d1e1e32d232e223c4daf3160140b7b983
5had0ww01f <xshadowwolfx1996@gmail.com>
2019-02-23 10:06:35 +0800

    Fix login view bug.

--- sanfu/urls.py
+++ sanfu/urls.py
@@ -1,40 +1,40 @@
 """sanfu URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
+from django.conf import settings
 from django.conf.urls import url
-from django.contrib.auth.views import login
-
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from django.contrib import admin
 from appointment.views import appointment_index
 from infomanage.views import infomanage_index
 from clinic.views import clinic_index
 from pharmacy.views import pharmacy_index
 from database.views import addPatient
-from dashboard.views import dashboard_index, google_check
+from dashboard.views import dashboard_index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/login/', login),
-    path('appointment/', appointment_index),
-    path('index/', dashboard_index),
-    path('infomanage/', infomanage_index),
-    path('database/', addPatient),
+    url(r'^accounts/login/$', LoginView.as_view()),
+    url(r'^admin/', admin.site.urls),
+    url(r'^appointment/', appointment_index),
+    url(r'^index/', dashboard_index),
+    url(r'^infomanage/', infomanage_index),
+    url(r'^database/', addPatient),
     url(r'^clinic/(.+)/$', clinic_index),
-    path('pharmacy/', pharmacy_index),
-    path('google9eb2f55447b43980.html', google_check),
-    path('', dashboard_index),
-]
+    url(r'^pharmacy/$', pharmacy_index),
+    #url(r'^upload/$', upload_file),
+    #url(r'^saved/', 'SaveProfile', name = 'saved'),
+    url(r'^$', dashboard_index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
